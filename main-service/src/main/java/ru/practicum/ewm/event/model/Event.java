43bd6653eaf3fbae979d6ba2742dfc71ceb78df4
Лосев Савелий <saveliy.losev@gmail.com>
2025-04-16 03:17:18 +0300

    Испраить ошибки

--- main-service/src/main/java/ru/practicum/ewm/event/model/Event.java
+++ main-service/src/main/java/ru/practicum/ewm/event/model/Event.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.event.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
